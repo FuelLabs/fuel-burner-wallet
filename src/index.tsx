4cb04ac9e4745f754fe2fc2d0cd64cb635d7631e
David Mihal <david@dmihal.com>
2020-03-30 00:29:04 -0300

    Add Fuel plugin to wallet

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { xdai, dai, eth } from '@burner-wallet/assets';
 import BurnerCore from '@burner-wallet/core';
-import { InjectedSigner, LocalSigner } from '@burner-wallet/core/signers';
-import { InfuraGateway, InjectedGateway, XDaiGateway, } from '@burner-wallet/core/gateways';
-import Exchange, { Uniswap, XDaiBridge } from '@burner-wallet/exchange';
+import { LocalSigner } from '@burner-wallet/core/signers';
 import ModernUI from '@burner-wallet/modern-ui';
+import { FuelGateway, FuelAsset, FuelPlugin } from 'fuel-burner-plugin';
+import fakedai from './fakedai.png';
 
-const core = new BurnerCore({
-  signers: [new InjectedSigner(), new LocalSigner()],
-  gateways: [
-    new InjectedGateway(),
-    new InfuraGateway(process.env.REACT_APP_INFURA_KEY),
-    new XDaiGateway(),
-  ],
-  // TODO use Sai
-  assets: [xdai, dai, eth],
+const fuelFakeDai = new FuelAsset({
+  id: 'fakeDai', 
+  name: 'FakeDai', 
+  network: '5', 
+  address: '0xCF852d1295fD158D43D58ceD47F88f4f4ab0931C', 
+  icon: fakedai, 
 });
 
-const exchange = new Exchange({
-  pairs: [new XDaiBridge(), new Uniswap('dai')],
+const core = new BurnerCore({
+  signers: [new LocalSigner()],
+  gateways: [new FuelGateway()],
+  assets: [fuelFakeDai],
 });
 
 const BurnerWallet = () =>
   <ModernUI
-    title="Basic Wallet"
+    title="Fuel Wallet"
     core={core}
-    plugins={[exchange]}
+    plugins={[
+      new FuelPlugin({ faucet: 'fakeDai' })
+    ]}
   />
 
 
